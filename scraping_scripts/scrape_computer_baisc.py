eb88b769550b9950f8d48a8e007bdc5587a076e9
Bruno Thalmann <mr.thalmann@gmail.com>
2015-10-24 13:02:25 +0200

    parsing script for basic computer abbreviations

--- scraping_scripts/scrape_computer_baisc.py
+++ scraping_scripts/scrape_computer_baisc.py
@@ -1,4 +1,11 @@
-from lxml import html
-import requests
+
+abbreviations = list()
+
+with open('../raw_data/basic_computer_abbreviations.txt', 'r') as f:
+    for line in f:
+        abbreviations.append(line.split('—'))
 
 
+with open('../database/basic_computer_abbreviations.txt', 'w') as f:
+    for item in abbreviations:
+        f.write(','.join(item))
